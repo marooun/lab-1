543d101b9dfb25f34a14c17f1952dcc657513f14
Maroun Hanna <maroun.hanna@lau.edu>
2023-01-27 15:34:47 +0200

    Initial info provided

---
+++ Profile.java
@@ -0,0 +1,7 @@
+public class Profile {
+	public static void main(String [] args) {
+	System.out.println("Name is Maroun Hanna");
+	System.out.println("Major is Computer Engineering");
+	System.out.println("First year 2023");
+}
+}
